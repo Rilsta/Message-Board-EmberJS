71a95f02d2108559d470cd0d8dca7bf771029b1c
rilsta <rstarnes9@gmail.com>
2016-04-01 11:00:26 -0700

    add ability to add answers to questions

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   reply: DS.attr(),
-  user: DS.attr()
-  // question: DS.belongsTo ('question', {async, true})
+  user: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
