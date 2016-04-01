0c20810f23339159dc5b2e3f85d52ab05f7983a6
rilsta <rstarnes9@gmail.com>
2016-04-01 09:17:25 -0700

    add ability to save new questions and view on homepage

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    save() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+      this.set('addNewQuestion', false);
+      this.set('content', "");
+      this.set('author', "");
+      this.set('notes', "");
+      this.sendAction("save", params);
+    }
+  }
 });
